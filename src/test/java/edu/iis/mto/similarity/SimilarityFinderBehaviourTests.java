1248a4de71652a14776e34c9eb45463962214e8b
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-03-30 11:59:56 +0200

    Added state and behavior test case declarations

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTests.java
@@ -0,0 +1,31 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+class SimilarityFinderBehaviourTests {
+
+    @Test
+    void checkIfFirstSetIsNullSecondNotNull() {
+
+    }
+
+    @Test
+    void checkIfFirstSetIsNotNullSecondNull() {
+
+    }
+
+    @Test
+    void checkIfBothSetsAreNull() {
+
+    }
+
+    @Test
+    void checkIfBothSetsAreNotNull() {
+
+    }
+
+    @Test
+    void verifyHowManyTimesSearchMethodIsCalled() {
+
+    }
+}
---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTests.java
@@ -0,0 +1,36 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+class SimilarityFinderStateTests {
+
+    @Test
+    void calculateJaccardIndexForEmptySetsTest() {
+
+    }
+
+    @Test
+    void calculateJaccardIndexForFirstSetEmptySecondNotEmptyTest() {
+
+    }
+
+    @Test
+    void calculateJaccardIndexForFirstSetNotEmptySecondEmptyTest() {
+
+    }
+
+    @Test
+    void calculateJaccardIndexForIdenticalSetsTest() {
+
+    }
+
+    @Test
+    void calculateJaccardIndexForSlightlyDifferentSetsTest() {
+
+    }
+
+    @Test
+    void calculateJaccardIndexForSignificantlyDifferentSetsTest() {
+
+    }
+}
