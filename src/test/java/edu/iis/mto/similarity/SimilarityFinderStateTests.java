2a5aec223782a5e3c9323f2083aa3a574d90be74
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-03-30 12:57:29 +0200

    Implemented state test case for both empty sets

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTests.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTests.java
@@ -1,12 +1,35 @@
 package edu.iis.mto.similarity;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderStateTests {
 
+    private SimilarityFinder similarityFinder;
+    private SequenceSearcherMock sequenceSearcherMock;
+    private static int[] emptySet = new int[0];
+    private static int[] multipleElementSet = {1, 2, 3};
+    private static int[] slightlyDifferentMultipleElementSet = {1, 2, 4};
+    private static int[] significantlyDifferentMultipleElementSet = {6, 3, 8};
+    private static final double IDENTICAL_SETS = 1.0d;
+    private static final double DISJOINT_SETS = 0.0d;
+    private static final double SIGNIFICANTLY_DIFFERENT_SETS = 0.2d;
+    private static final double SLIGHTLY_DIFFERENT_SETS = 0.5d;
+
+    @BeforeEach
+    void setup() {
+        sequenceSearcherMock = new SequenceSearcherMock();
+        similarityFinder = new SimilarityFinder(sequenceSearcherMock);
+    }
+
     @Test
     void calculateJaccardIndexForEmptySetsTest() {
-
+        double jaccardIndex = similarityFinder.calculateJackardSimilarity(emptySet, emptySet);
+        assertThat(jaccardIndex, is(equalTo(IDENTICAL_SETS)));
     }
 
     @Test
