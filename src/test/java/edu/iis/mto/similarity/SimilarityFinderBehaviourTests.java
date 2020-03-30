e7955e032ae1aab50ef55f0f43fa055207f976f6
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-03-30 12:25:59 +0200

    Implemented test case for first set, which is null and second set, which is not null

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTests.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTests.java
@@ -1,12 +1,25 @@
 package edu.iis.mto.similarity;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimilarityFinderBehaviourTests {
 
+    private SimilarityFinder similarityFinder;
+    private SequenceSearcherMock sequenceSearcherMock;
+    private static int[] notNullSet = {1, 2, 3};
+
+    @BeforeEach
+    void setup() {
+        sequenceSearcherMock = new SequenceSearcherMock();
+        similarityFinder = new SimilarityFinder(sequenceSearcherMock);
+    }
+
     @Test
     void checkIfFirstSetIsNullSecondNotNull() {
-
+        assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, notNullSet));
     }
 
     @Test
