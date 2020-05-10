80e7a091cccfe84a71eddc29ffd5fa57af690a04
Patryk Stępień <patryk.stepien@automotivedev.pl>
2020-05-10 03:16:01 +0200

    Removed algorithm from SequenceSearcherMock and update gitignore

--- src/test/java/edu/iis/mto/similarity/SequenceSearcherMock.java
+++ src/test/java/edu/iis/mto/similarity/SequenceSearcherMock.java
@@ -1,14 +1,25 @@
 package edu.iis.mto.similarity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import edu.iis.mto.search.SearchResult;
 import edu.iis.mto.search.SequenceSearcher;
 
 public class SequenceSearcherMock implements SequenceSearcher {
 
     private int searchMethodCallNumber = 0;
+    private Map<Integer, Integer> secondSeq = new HashMap<>();
 
     public int getSearchMethodCallNumber() {
         return searchMethodCallNumber;
+    }
+
+    public void setSecondSeq(int[] secondSeq) {
+        int i = 0;
+        for (int elem : secondSeq) {
+            this.secondSeq.put(elem, i++);
+        }
     }
 
     @Override
@@ -17,15 +28,13 @@ public class SequenceSearcherMock implements SequenceSearcher {
         searchMethodCallNumber++;
 
         SearchResult.Builder builder = SearchResult.builder();
-        builder.withFound(false)
-               .withPosition(-1);
 
-        for (int i = 0; i < seq.length; i++) {
-            if (seq[i] == key) {
-                builder.withFound(true)
-                       .withPosition(i);
-                break;
-            }
+        if (secondSeq.containsKey(key)) {
+            builder.withFound(true)
+                   .withPosition(secondSeq.get(key));
+        } else {
+            builder.withFound(false)
+                   .withPosition(-1);
         }
 
         return builder.build();
